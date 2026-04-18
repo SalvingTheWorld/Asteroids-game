24d9a7c0a2012f296de6d8a291fa51866b57479a
Sal <juniormoraes2012@gmail.com>
2026-04-18 02:20:41 -0300

    new changes to the game and addition of the player

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+# The base class for all game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # overrided in the Player class
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,14 +10,28 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group() #holds all objects that can be updated
+    drawable = pygame.sprite.Group() #holds all objects that can be drawn
+    Player.containers = (updatable, drawable)
+
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill('black')
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
